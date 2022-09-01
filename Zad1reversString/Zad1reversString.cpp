bc4693cf4bcdbcf88c28794ed94765e88be3d507
Klaudia <97130591+luikada@users.noreply.github.com>
2022-09-01 15:23:31 +0200

    Add files via upload

---
+++ Zad1reversString/Zad1reversString.cpp
@@ -0,0 +1,21 @@
+﻿//Napisz funkcję, która odwróci podanego stringa.
+
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+std::string Reversing(std::string &s)
+{
+
+    std::reverse(s.begin(), s.end());
+    return s;
+}
+
+int main()
+{
+    std::string hW= "Hello World!";
+    auto res = Reversing(hW);
+    std::cout << res; 
+}
+
---
+++ Zad2Twostrings/Zad2Twostrings.cpp
@@ -0,0 +1,30 @@
+﻿// Zad2Twostrings.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+
+int mutualLetters(std::string s1, std::string s2)
+{
+    std::set<char>catcher;
+    
+    for (int i = 0; i < s1.size(); i++)
+    {
+        if (s1[i] == s2[i])
+        {
+            catcher.insert(s1[i]);
+        }
+    }
+    return catcher.size();
+}
+
+int main()
+{
+    std::string k = "kasia";
+    std::string b = "basia";
+  auto result=  mutualLetters(k, b);
+    std::cout << result; 
+}
+
