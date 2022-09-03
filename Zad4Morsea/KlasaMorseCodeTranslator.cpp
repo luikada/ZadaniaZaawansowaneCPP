2fdb19f9f179f824114b966b97389e220158cb3f
Klaudia <97130591+luikada@users.noreply.github.com>
2022-09-03 09:02:21 +0200

    Add files via upload

--- Zad4Morsea/KlasaMorseCodeTranslator.cpp
+++ Zad4Morsea/KlasaMorseCodeTranslator.cpp
@@ -1,4 +1,5 @@
 #include "KlasaMorseCodeTranslator.h"
+#include <iostream>
 
 KlasaMorseCodeTranslator::KlasaMorseCodeTranslator()
 {
@@ -30,18 +31,69 @@ KlasaMorseCodeTranslator::KlasaMorseCodeTranslator()
 	
 	};
 
+	MtoA = {
+		{ ".- ", 'A',},
+		{"-...", 'B'  },
+		{"-.-." , 'C'},
+		{"-..", 'D' },
+		{".",  'E' },
+		{"..-.",  'F' },
+		{"--." , 'G'},
+		{ "...." ,'H' },
+		{"..",  'I'  },
+		{".---", 'J' },
+		{ "-.- ", 'K',},
+		{".-..", 'L'  },
+		{ "--", 'M'  },
+		{"-." ,'N' },
+		{ "---",'O'  },
+		{ ".--." , 'P'},
+		{ ".-.",'R' },
+		{"...", 'S'  },
+		{"-", 'T' },
+		{ "..-",'U'  },
+		{ ".--",'W'  },
+		{"-..-", 'X' },
+		{"-.--",  'Y'  },
+		{ "--..",'Z'  },
+	
+	};
+
 }
 
 std::vector<std::string> KlasaMorseCodeTranslator::toMorseCode(std::string t)
 {
-	std::string wyraz;
 	std::vector <std::string> newWord;
 
-	for (int i = 0; i < wyraz.size(); i++)
+	for (int i = 0; i < t.size(); i++)
 	{
-		char c = wyraz[i];
+		char c = t[i];
 		AtoM[c];
 		newWord.push_back(AtoM[c]);
+		newWord.push_back(", ");
 	}
 	return newWord;
+}
+
+std::string KlasaMorseCodeTranslator::fromMorse(std::string t) //--- ... ---
+{
+
+	std::string newWordfromM;
+	std::string newLetter;
+
+	for (int i = 0; i < t.size(); ++i)
+	{
+		if ((t[i] == '.')|| (t[i]=='-'))
+		{
+			newLetter.push_back(t[i]);
+		}
+		else
+		{
+			char c = MtoA[newLetter];
+			newWordfromM.push_back(c);
+			newLetter.clear();
+		}
+
+	}
+	return newWordfromM;
 }
--- Zad4Morsea/KlasaMorseCodeTranslator.h
+++ Zad4Morsea/KlasaMorseCodeTranslator.h
@@ -2,14 +2,17 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <algorithm>
 class KlasaMorseCodeTranslator
 { public:
 
 	KlasaMorseCodeTranslator();
 
 	std::vector<std::string>toMorseCode(std::string t);
+	std::string fromMorse(std::string t);
 private:
 	std::map<char, std::string> AtoM;
+	std::map< std::string, char> MtoA;
 
 };
 
