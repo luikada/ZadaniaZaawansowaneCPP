3987d4e20d95f80bcf5ecb1854214313b5bc3625
Klaudia <97130591+luikada@users.noreply.github.com>
2022-09-13 00:06:37 +0200

    Add files via upload

---
+++ Rectangle.h
@@ -0,0 +1,27 @@
+//// Zadanie 8 -to
+//Klasy: Rectangle(przechowuje d³ugoœci boków, wylicza pole, isSquare()), RectangleGenerator(Klasa genreuj¹ca losowy prostok¹t o boku z podanego przedzia³u), 
+//RectangleManger(tutaj faktycznie implementujemy zadania)
+//Utwórz wektor 100 prostok¹tów o losowych d³ugoœciach boków z przedzia³u 0, 10 (u¿yj liczb ca³kowitych)
+//a) void printRectangles() - Wypisz wszystkie prostok¹ty(for_each + unary function)
+//b) size_t countRectangleBiggerThen(int area) - Wypisz ile jest prostok¹tów o polu wiêkszym ni¿ 50 (count_if)
+//c) std::vector copyRectangles() - Przekopiuj wszystkie prostk¹ty, które s¹ kwadratami do drugiego vectora(copy_if) i go wypisz(for_each)
+//d) void sortAreaDescending() - Posortuj vector prostok¹tów malejaco wed³ug pola powierzchni
+//e) void deleteInvalid() Usuñ wszystkie prostok¹ty, które maj¹ przynajmniej jeden bok równy 0
+
+class Rectangle 
+{
+private: 
+	double _sideA;
+	double _sideB;
+
+public:
+	Rectangle(double a, double b);
+	double getSideA();
+	double getSideB();
+	double getArea(); 
+	double getCircuit();
+	bool isSquare(); 
+
+
+};
+
---
+++ RectangleGenerator.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <random>
+#include "Rectangle.h"
+#include <iostream>
+//RectangleGenerator(Klasa genreuj¹ca losowy prostok¹t o boku z podanego przedzia³u),
+
+class RectangleGenerator :Rectangle
+{
+private: 
+	int _min, _max;
+	double _side;
+
+	std::mt19937 engine;
+	std::uniform_int_distribution<int> dist;
+
+public:
+	
+	RectangleGenerator(int min, int max);
+	double createSide();
+	Rectangle createRectangle();
+};
+  
